1b70548e7ac904d26bd69c49b15a51fa8d000bc6
bubumariia <mariiakudaiberdieva@gmail.com>
2025-07-17 14:29:00 +0300

    building RESTful APIs with Express.js

--- client.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket() # Create a socket object
-port = 12345 # Same IP port to connect to
-# connect to the server on local computer
-s.connect(( '127.0.0.1' , port))
-# receive data from the server and decode it to get as string.
-print (s.recv( 1024).decode())
-s.close() # close the connection
