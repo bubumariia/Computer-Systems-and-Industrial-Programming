8b2a62fcea4c7d0dbd85fa0d5c48816cfce07cda
bubumariia <mariiakudaiberdieva@gmail.com>
2025-07-19 19:45:16 +0300

    Node.js API

--- HW/crossfit-wod-api/src/index.js
+++ HW/crossfit-wod-api/src/index.js
@@ -1,14 +1,12 @@
-// In src/index.js 
-const express = require("express"); 
+const express = require("express");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
-const app = express(); 
-const PORT = process.env.PORT || 3000; 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// For testing purposes 
-app.get("/", (req, res) => { 
-    res.send("<h2>It's Working, hoooray!</h2>"); 
-}); 
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+  console.log(`✅ API is listening on port ${PORT}`);
 });
